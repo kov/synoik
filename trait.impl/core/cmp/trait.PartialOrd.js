55eea9b34a59ac4b0a96bd9b8a7f0c84273ae7d3
kov <kov@users.noreply.github.com>
2026-08-20 12:56:50 +0000

    deploy: e8e6a7786c83567f1eb0898117fc8937cd369a29

---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["synoik_ipc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"synoik_ipc/enum.Layer.html\" title=\"enum synoik_ipc::Layer\">Layer</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[273]}
